898b208a8aa1c150fab0f30a85b5098b875480f9
iamahmedshahh <artistahmed41@gmail.com>
2023-09-06 03:46:41 +0500

    updates

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
